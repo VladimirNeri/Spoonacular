602b7ad52202409bbdd85c86cb346c0bb4bf498b
Vlad Laptop <vlad.neri@gmail.com>
2021-08-19 15:56:57 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
