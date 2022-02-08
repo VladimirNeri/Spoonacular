0066f657220e4c78f0d85ae9b487ad181491f5b8
VladimirNeri <vlad.neri@gmail.com>
2022-02-07 22:52:12 -0800

    added nutritionfacts

--- src/pages/recipesearch.jsx
+++ src/pages/recipesearch.jsx
@@ -5,24 +5,38 @@ import {
   Route,
   useParams,
   useRouteMatch,
-} from "react-router-dom";
+} from 'react-router-dom';
 import spoonacular from '../apis/spoonacular';
 import RecipeDetails from '../components/RecipeDetails';
-
-
 
 const RecipeSearch = () => {
   const { id } = useParams();
   const [recipeData, setRecipeData] = useState([]);
+  const [nutritionLabel, setNutritionLabel] = useState([]);
   const match = useRouteMatch();
 
   useEffect(() => {
     const fetchData = async () => {
       const apiKey = process.env.REACT_APP_API_KEY;
-      const result = await spoonacular.get(
-        `/recipes/${id}/information?includenutrition=false?&apiKey=${apiKey}`
-      );
+      const [result, nutrition] = await Promise.all([
+        spoonacular.get(
+          `/recipes/${id}/information?includenutrition=false?&apiKey=${apiKey}`
+        ),
+        spoonacular
+          .get(`/recipes/${id}/nutritionLabel.png?&apiKey=${apiKey}`, {
+            responseType: 'blob',
+            headers: {
+              'Content-Type': 'image/png',
+            },
+          })
+          .then((response) => {
+            let imgUrl = URL.createObjectURL(response.data);
+            return imgUrl;
+          }),
+      ]);
       setRecipeData(result.data);
+      setNutritionLabel(nutrition);
+      console.log(nutrition);
       console.log(result.data);
     };
     fetchData();
@@ -33,8 +47,8 @@ const RecipeSearch = () => {
       <h2>{recipeData.title}</h2>
       <img
         src={recipeData.image}
-        width="470"
-        className="recipeData-image"
+        width='470'
+        className='recipeData-image'
         alt={recipeData.name}
       />
 
@@ -47,13 +61,36 @@ const RecipeSearch = () => {
               </NavLink>
             </li>
             <li>
-              <NavLink exact activeClassName='tab-active' to={match.url + '/ingredients-list'}>
+              <NavLink
+                exact
+                activeClassName='tab-active'
+                to={match.url + '/ingredients-list'}
+              >
                 Ingredients
               </NavLink>
             </li>
             <li>
-              <NavLink exact activeClassName='tab-active' to={match.url + '/instructions'}>
+              <NavLink
+                exact
+                activeClassName='tab-active'
+                to={match.url + '/instructions'}
+              >
                 Instructions
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                exact
+                activeClassName='tab-active'
+                to={match.url + '/nutrition'}
+              >
+                Nutrition
+                <img
+                  src={nutritionLabel}
+                  alt='Nutrition Facts'
+                  className='nutrition-facts-png'
+                  width='275'
+                />
               </NavLink>
             </li>
           </ul>
