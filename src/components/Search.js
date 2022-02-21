a6a887506bea42841c0cb8da7b7f10858d3c868f
VladimirNeri <vlad.neri@gmail.com>
2022-02-20 18:20:32 -0800

    update css

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,36 +1,39 @@
 import React, { useState } from 'react';
 import './search.css';
 
-
 const Search = (props) => {
   const [search, setSearch] = useState('');
-  
 
   const onChanges = (e) => {
     setSearch(e.target.value);
-  }
+  };
 
   const resetInputField = () => {
-    setSearch('')
-  }
+    setSearch('');
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.search(search);
     resetInputField();
-  }
+  };
 
   return (
-      <form className='recipe-search'>
-        <input
-          className='ingredient-input'
-          type='text'
-          placeholder='Search Recipe by Ingredient'
-          value={search}
-          onChange={onChanges}
-        />
-        <input onClick={handleSubmit} type="submit" value="SEARCH" className='search-button'/>
-      </form>
+    <form className='recipe-search'>
+      <input
+        className='ingredient-input'
+        type='text'
+        placeholder='Search Recipe by Ingredient'
+        value={search}
+        onChange={onChanges}
+      />
+      <input
+        onClick={handleSubmit}
+        type='submit'
+        value='SEARCH'
+        className='btn'
+      />
+    </form>
   );
 };
 
